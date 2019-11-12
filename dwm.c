6ded73797fdcedc5733cedd96e70d85c6331724b
Soenke Lambert <s.lambert@mittwald.de>
2019-11-12 10:44:02 +0100

    Fullscreen current window with [Alt]+[Shift]+[f]
    This actually fullscreens a window, instead of just hiding the statusbar
    and applying the monocle layout.

--- dwm.c
+++ dwm.c
@@ -216,6 +216,7 @@ static void tile(Monitor *);
 static void togglealttag();
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
+static void togglefullscr(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
 static void unfocus(Client *c, int setfocus);
@@ -1751,6 +1752,13 @@ togglefloating(const Arg *arg)
 		resize(selmon->sel, selmon->sel->x, selmon->sel->y,
 			selmon->sel->w, selmon->sel->h, 0);
 	arrange(selmon);
+}
+
+void
+togglefullscr(const Arg *arg)
+{
+  if(selmon->sel)
+    setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
 }
 
 void
